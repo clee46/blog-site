c2919d4fa2a47a87acd480b74da123d824c082f4
Christopher Lee <yensen@gmail.com>
2015-12-02 13:47:37 -0800

    implemented tabs without jquery-ui, fixed show/hide article toggle bug

--- blog.js
+++ blog.js
@@ -18,7 +18,7 @@ blog.truncateArticles = function() {
   $('article .postBody p:not(:first-child)').hide();  // hides all posts
   $('main').on('click', '.read-on', function(event) {
    event.preventDefault();
-   $(this).parent().find('p:not(:first-child)').show();
+  $(this).siblings('.postBody').find('p:not(:first-child)').toggle();
  });
 };
 
@@ -27,25 +27,42 @@ $(document).ready(function() {
   $('#post').remove();    // remove the original article template
   blog.truncateArticles();
 
+  $('.nav-tabs > li > a').click(function(event){
+  		event.preventDefault();//stop browser to take action for clicked anchor
+
+  		//get displaying tab content jQuery selector
+  		var active_tab_selector = $('.nav-tabs > li.active > a').attr('href');
+
+  		//find actived navigation and remove 'active' css
+  		var actived_nav = $('.nav-tabs > li.active');
+  		actived_nav.removeClass('active');
+
+  		//add 'active' css into clicked navigation
+  		$(this).parents('li').addClass('active');
+
+  		//hide displaying tab content
+  		$(active_tab_selector).removeClass('active');
+  		$(active_tab_selector).addClass('hide');
+
+  		//show target tab content
+  		var target_tab_selector = $(this).attr('href');
+  		$(target_tab_selector).removeClass('hide');
+  		$(target_tab_selector).addClass('active');
+  });
+  // event handler for category filter menu
   $('select[id="category"]').change(function(){
     $('#author').find('option:first').attr('selected', 'selected'); // reset other menu
     $('main').find('article').show();
-    console.log($(this).val());
-    // if ($(this).val() !== 'none'){$('main').find('article:not(.' + $(this).val() + ')').hide();}
     if ($(this).val() !== 'none'){
       $('.postCategory:not(:contains(' + $(this).val() + '))').parent().hide();
     }
   });
-
+  // event handler for author filter menu
   $('select[id="author"]').change(function(){
     $('#category').find('option:first').attr('selected', 'selected'); // reset other menu
     $('main').find('article').show();
-    console.log($(this).val());
     if ($(this).val() !== 'none'){
       $("article:not(:contains(" + $(this).val() + "))").hide();
     }
-    // $('main').find('article:not(p.postAuthor:contains(' + $(this).val() + '))').hide;
   });
-
-  $('#navBar').accordion();
 });
