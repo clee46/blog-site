5466b1d1b5b423114de452480aa34ca16ec7c233
Christopher Lee <yensen@gmail.com>
2015-12-02 12:40:17 -0800

    small syntax change

--- blog.js
+++ blog.js
@@ -3,9 +3,9 @@ blog.library = [];
 
 blog.createAll = function() {
   this.rawData.sort(function (a, b) {
-  if (a.publishedOn > b.publishedOn) {return -1;}
-  if (a.publishedOn < b.publishedOn) {return 1;}
-  return 0;
+    if (a.publishedOn > b.publishedOn) {return -1;}
+    if (a.publishedOn < b.publishedOn) {return 1;}
+    return 0;
   });
   for (var i = 0; i < this.rawData.length; i++) {
     var temp = new Article(this.rawData[i]);
