993efa7e977662ee111cb57a7b348ebfd816122b
Christopher Lee <yensen@gmail.com>
2015-12-16 14:57:24 -0800

    got server.js to work, also added call to webDB.setupTables

--- scripts/repos.js
+++ scripts/repos.js
@@ -4,9 +4,17 @@ repos.all = [];
 
 repos.requestAll = function(callback) {
   $.ajax({
+    url: '/github/users/clee46/repos',
     type: 'GET',
-    url: '/github/users/clee46'
-  }).done(function(data) {
-    repos.all = data;
+    success:  function(data, message, xhr) {
+      repos.all = data;
+    }
   }).done(callback);
 };
+
+//   done(function(data) {
+//     console.log('data is: ');
+//     console.log(data);
+//     repos.all = data;
+//   }).done(callback);
+// };
